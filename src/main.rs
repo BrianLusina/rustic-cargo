8946501b679e15a623270a4fcab38d1761356452
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-10-11 13:35:43 +0300

    chore: tuple and array basics

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,49 @@ fn main() {
     println!("I now have {} apples", more_apples);
     // some arithmetic
     println!("Let's do some arithmetic");
-    let answer = 6 / 2+ 4*3;
-    println!("Answer is {}. Following standard Math operations(BODMAS)", answer);
+    let answer = 6 / 2 + 4 * 3;
+    println!(
+        "Answer is {}. Following standard Math operations(BODMAS)",
+        answer
+    );
     let another_answer = ((6 / 2) + 4) * 3;
-    println!("Another Answer is {}. Following standard Math operations (BODMAS) with brackets", another_answer);
+    println!(
+        "Another Answer is {}. Following standard Math operations (BODMAS) with brackets",
+        another_answer
+    );
 
-    let number = if apples < 10 {
-      apples
-    } else {
-     apples
-    };
-    println!("Number: {}", number)
+    let number = if apples < 10 { apples } else { apples };
+    println!("Number: {}", number);
+
+    // tuple
+    let tup = (90, "ser", 'a');
+    // destructuring
+    let (x, y, z) = tup;
+    // index access
+    let ninety = tup.0;
+    let set = tup.1;
+    let a = tup.2;
+
+    // arrays
+    let coll: [i32; 4] = [1, 2, 3, 4];
+    let coll2 = [5; 5]; // all positions in the array will be 5, a concise way of writing an array
+
+    // element access of array with indices
+    let first = coll2[0];
+    let second = coll2[3];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 }
