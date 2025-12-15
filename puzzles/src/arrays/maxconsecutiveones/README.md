362d826c8d99486b07061949bc7ae228e8ae7147
Lusina <12752833+BrianLusina@users.noreply.github.com>
2025-12-15 10:33:01 +0300

    refactor(puzzles, max consecutive ones): max consecutive ones in binary array

---
+++ puzzles/src/arrays/maxconsecutiveones/README.md
@@ -0,0 +1,15 @@
+# Max consecutive ones
+
+You are given a binary array nums (an array that contains only 0s and 1s). Your task is to find the maximum number of consecutive 1s in the array and return it.
+
+## Constraints
+
+- 1 ≤ `nums.length` ≤ 10^3
+- `nums[i` is either 0 or 1
+
+## Related Topics
+
+- Array
+- Binary Search
+- Sliding Window
+- Prefix Sum
---
+++ puzzles/src/arrays/maxconsecutiveones/max_consecutive_ones.rs
@@ -0,0 +1,29 @@
+pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+    if nums.len() == 0 {
+        return 0;
+    }
+    
+    let mut max_consecutive_ones = 0;
+    let mut current_consecutive_ones = 0;
+    
+    for num in nums {
+        if num == 1 {
+            current_consecutive_ones += 1;
+            max_consecutive_ones = max_consecutive_ones.max(current_consecutive_ones);
+        } else {
+            current_consecutive_ones = 0;
+        }
+    }
+
+    max_consecutive_ones
+}
+
+#[cfg(test)]
+mod find_max_consecutive_ones_tests {
+    use super::*;
+    
+    #[test]
+    fn test_find_max_consecutive_ones() {
+        assert_eq!(find_max_consecutive_ones(vec![1,1,0,1,1,1]), 3);
+    }
+}
